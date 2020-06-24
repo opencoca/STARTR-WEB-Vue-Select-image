b46e685ce82110b007b6257071585755c09e79a9
OpenCo <admin@openco.ca>
2020-06-24 10:32:28 -0400

    Moving assets to public

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,7 @@ export default {
       logo: [
         {
           id: 1,
-          src:
-            "https://i.pinimg.com/originals/0c/67/5a/0c675a8e1061478d2b7b21b330093444.gif"
+          src: "/assets/robot.gif"
         }
       ],
       message: "Hello! This should allow image based selection!",
@@ -31,25 +30,23 @@ export default {
         {
           id: 1,
           src:
-            "https://cdn.icon-icons.com/icons2/2416/PNG/512/circle_doodle_heart_plate_shape_valentine_favorite_button_icon_146721.png",
+            "/assets/circle_doodle_heart_plate_shape_valentine_favorite_button_icon_146721.png",
           alt: "Love things and love your self!"
         },
         {
           id: 2,
-          src:
-            "https://cdn.icon-icons.com/icons2/2416/PNG/512/heart_list_task_to_do_icon_146658.png",
+          src: "/assets/heart_list_task_to_do_icon_146658.png",
           alt: "Keep bullet journaling!"
         },
         {
           id: 3,
           src:
-            "https://cdn.icon-icons.com/icons2/2416/PNG/512/alcohol_bar_bottle_champagne_drink_romantic_toast_icon_146713.png",
+            "/assets/alcohol_bar_bottle_champagne_drink_romantic_toast_icon_146713.png",
           alt: "Take it easy ❦"
         },
         {
           id: 4,
-          src:
-            "https://cdn.icon-icons.com/icons2/2416/PNG/512/draw_edit_heart_line_pen_writing_icon_146731.png",
+          src: "/assets/draw_edit_heart_line_pen_writing_icon_146731.png",
           alt: "Always write things down."
         }
       ]
@@ -57,7 +54,7 @@ export default {
   },
   methods: {
     onSelectImage: function(image) {
-      this.message = "You clicked image " + image.id + "! <br>" + image.alt;
+      this.message = "You clicked image " + image.id + "! \n " + image.alt;
     }
   }
 };
